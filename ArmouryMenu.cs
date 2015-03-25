c061d5144ba260fc0d7dc3bf3259d95957dabe04
Philosoraptr <Philosoraptr@users.noreply.github.com>
2015-03-26 09:14:30 +1300

    Update ArmouryMenu.cs

--- ArmouryMenu.cs
+++ ArmouryMenu.cs
@@ -6,17 +6,21 @@ public class ArmouryMenu : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
+		//sort of pseudo code at this stage
+		for int i = 1 to numberOfSpriteSheets
+		{
+			buttonLabel = SpriteSheet.Name;
+			Sprite Sprites = Resources.Load <Sprite> ("Characters/" + spriteSheetName + "/Sprite_07");
+			buttonImage = Sprite;
+		}
+		
 		var subSprites = Resources.LoadAll<Sprite> ("Characters/" + spriteSheetName);
 		
 		foreach(var renderer in GetComponentsInChildren<SpriteRenderer>())
 		{
 			string spriteName = renderer.sprite.name;
 			var newSprite = Array.Find(subSprites, item => item.name == spriteName);
-			
-			if(newSprite)
-			{
-				renderer.sprite = newSprite;
-			}
+		}
 	}
 	
 	// Update is called once per frame

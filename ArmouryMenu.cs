f8fa9a60e47b9269e8b5503021abb9be05f10ab4
Philosoraptr <benj.smith19@gmail.com>
2015-03-26 21:59:57 +1300

    Updated ArmouryMenu
    Updated ArmouryMenu with code for looping through filenames in
    Resources/Characters. This will subsequently be used to loop through
    file locations to grab the sprite_x image for populating into the
    armoury buttons.

--- ArmouryMenu.cs
+++ ArmouryMenu.cs
@@ -1,30 +1,57 @@
-﻿using UnityEngine;
+﻿// Sources
+// http://stackoverflow.com/questions/4804990/c-sharp-getting-file-names-without-extensions
+// http://stackoverflow.com/questions/26187800/unity-4-6-button-instances-added-at-runtime-are-not-scaled-by-reference-resolut
+// http://stackoverflow.com/questions/27503262/instantiate-method-doesnt-draw-the-prefab-button-inside-the-canvas-and-doesnt
+// http://docs.unity3d.com/Manual/InstantiatingPrefabs.html
+
+using UnityEngine;
 using System.Collections;
+using System.IO;
 
 public class ArmouryMenu : MonoBehaviour {
+
+	public GameObject armouryButton;
+	public float spacing = 150f;
 
 	// Use this for initialization
 	void Start () 
 	{
-		//sort of pseudo code at this stage
-		for int i = 1 to numberOfSpriteSheets
+		DirectoryInfo dir = new DirectoryInfo("Assets/Resources/Characters");
+		FileInfo[] info = dir.GetFiles("*.*");
+		foreach (FileInfo f in info) 
 		{
-			buttonLabel = SpriteSheet.Name;
-			Sprite Sprites = Resources.Load <Sprite> ("Characters/" + spriteSheetName + "/Sprite_07");
-			buttonImage = Sprite;
+			if(f.Extension == ".png")
+			{
+				Debug.Log(Path.GetFileNameWithoutExtension(f.Name));
+			}
 		}
-		
-		var subSprites = Resources.LoadAll<Sprite> ("Characters/" + spriteSheetName);
-		
-		foreach(var renderer in GetComponentsInChildren<SpriteRenderer>())
-		{
-			string spriteName = renderer.sprite.name;
-			var newSprite = Array.Find(subSprites, item => item.name == spriteName);
-		}
+
+//		int i = 0;
+//		Sprite[] subSprites = Resources.LoadAll<Sprite>("Characters");
+//		Debug.Log (subSprites.Length);
+
+
+//		foreach(Sprite sprite in subSprites)
+//		{
+//			//string spriteName = sprite.name;
+////			Instantiate(armouryButton, new Vector3(220f, (800f + (spacing * i)), 0f), Quaternion.identity);
+//			armouryButton = (GameObject)GameObject.Instantiate(armouryButton, transform.position, Quaternion.identity);
+//			armouryButton.transform.SetParent(gameObject.transform, false);
+//			i += 1;
+//		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 	
 	}
 }
+
+//sort of pseudo code at this stage
+//		for int i = 1 to numberOfSpriteSheets
+//		{
+//			buttonLabel = SpriteSheet.Name;
+//			Sprite Sprites = Resources.Load <Sprite> ("Characters/" + spriteSheetName + "/Sprite_07");
+//			buttonImage = Sprite;
+//		}
